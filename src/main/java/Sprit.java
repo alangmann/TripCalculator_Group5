cd124ad726f13e7a860dd28b7bad4367ce1fe261
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-11-27 11:58:25 +0100

    Added Sprit Class

---
+++ src/main/java/Sprit.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Juergen on 27.11.2014.
+ */
+public class Sprit {
+
+    private String weekDay;
+    private double patrolPrice;
+    private double dieselPrice;
+
+    public Sprit(String weekDay, double patrolPrice, double dieselPrice) {
+        this.weekDay = weekDay;
+        this.patrolPrice = patrolPrice;
+        this.dieselPrice = dieselPrice;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public double getPatrolPrice() {
+        return patrolPrice;
+    }
+
+    public void setPatrolPrice(double patrolPrice) {
+        this.patrolPrice = patrolPrice;
+    }
+
+    public double getDieselPrice() {
+        return dieselPrice;
+    }
+
+    public void setDieselPrice(double dieselPrice) {
+        this.dieselPrice = dieselPrice;
+    }
+}
