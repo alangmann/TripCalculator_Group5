d82833df96a1e2d60756848572664b17c0600781
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-11-27 12:16:22 +0100

    Test Class Add after some fails

---
+++ src/test/java/TripCalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class TripCalculatorTest {
+    private TripCalculator tc;
+
+    @Before
+    public void setUp()
+    {
+        tc = new TripCalculator();
+    }
+}
