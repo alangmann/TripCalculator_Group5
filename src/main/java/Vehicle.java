bff136f6ced524c4789be9c6b021e5a4a52d0842
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-12-04 12:03:20 +0100

    Implement Vehicle

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -3,4 +3,36 @@
  */
 public class Vehicle {
     private double averageConsumption;
+    private FuelType typeOfFuel;
+    private int cargo;
+
+    public Vehicle(double averageConsumption, FuelType typeOfFuel, int cargo) {
+        this.averageConsumption = averageConsumption;
+        this.typeOfFuel = typeOfFuel;
+        this.cargo = cargo;
+    }
+
+    public double getAverageConsumption() {
+        return averageConsumption;
+    }
+
+    public void setAverageConsumption(double averageConsumption) {
+        this.averageConsumption = averageConsumption;
+    }
+
+    public FuelType getTypeOfFuel() {
+        return typeOfFuel;
+    }
+
+    public void setTypeOfFuel(FuelType typeOfFuel) {
+        this.typeOfFuel = typeOfFuel;
+    }
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
+    }
 }
