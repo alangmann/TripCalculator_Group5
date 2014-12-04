550c119fea7e818ae6d50ce1a238765be492691a
korjuc11 <korjuc11@htlkaindorf.at>
2014-12-04 12:10:09 +0100

    car and truck written

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,4 +2,10 @@
  * Created by Juergen on 04.12.2014.
  */
 public class Car extends Vehicle {
+
+    public Car(double averageConsumption, FuelType typeOfFuel, int cargo) {
+        super(averageConsumption, typeOfFuel, cargo);
+    }
+
+
 }
--- src/main/java/Truck.java
+++ src/main/java/Truck.java
@@ -6,9 +6,26 @@ public class Truck extends Vehicle {
     private int axles;
     private boolean adBlue;
 
-    public Truck(int axles, boolean adBlue) {
+    public Truck(double averageConsumption, FuelType typeOfFuel, int cargo,int axles, boolean adBlue) {
+        super( averageConsumption,  typeOfFuel,  cargo);
         this.axles = axles;
         this.adBlue = adBlue;
+    }
 
+
+    public int getAxles() {
+        return axles;
+    }
+
+    public void setAxles(int axles) {
+        this.axles = axles;
+    }
+
+    public boolean isAdBlue() {
+        return adBlue;
+    }
+
+    public void setAdBlue(boolean adBlue) {
+        this.adBlue = adBlue;
     }
 }
