8a8d8f6000ac05a09c31900856e41e5a48254743
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-12-04 10:53:46 +0100

    calculate on distance method COMMIT

--- src/main/java/TripCalculator.java
+++ src/main/java/TripCalculator.java
@@ -12,24 +12,33 @@ public class TripCalculator {
 
 
     public void readRoutesCSV() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\trunk\\src\\main\\resources\\routes.csv"));
+        BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\trunk\\src\\main\\resources\\routes.csv"));
         String line = "";
         int count = 0;
         while ((line = br.readLine()) != null) {
             String[] parts = line.split(";");
-            if(count>0)
-            {
+            if (count > 0) {
                 int km = Integer.parseInt(parts[0]);
                 int slope = Integer.parseInt(parts[1]);
                 String routeType = parts[2];
                 double specialFee = Double.parseDouble(parts[3]);
 
-                routeList.add(new Routes(km,slope,routeType,specialFee));
-
+                routeList.add(new Routes(km, slope, routeType, specialFee));
             }
             System.out.println(line);
             count++;
         }
+    }
+
+    public double calculateCO2onDistance()
+    {
+        double co2value = 0;
+        for(Routes route : routeList)
+        {
+            //0.1325 = CO2 Wert für 5l/100km
+            co2value += route.getKm() + 0.1325;
+        }
+        return co2value;
     }
 
     public static void main(String[] args) {
