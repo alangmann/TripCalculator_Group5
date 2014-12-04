7e2ac356eef3421573b928d1f59fc9ae1621802d
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-12-04 11:43:16 +0100

    Added Vehicle, Truck, Car Classes and FuelType Enum

---
+++ src/main/java/FuelType.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Juergen on 04.12.2014.
+ */
+public enum FuelType {
+    Diesel, Patrol
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Juergen on 04.12.2014.
+ */
+public class Vehicle {
+}
