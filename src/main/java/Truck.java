4cca40e7d30d6e25b3a93e5a652231592ff09ddb
korjuc11 <korjuc11@htlkaindorf.at>
2014-12-04 12:03:52 +0100

    MinorChanges

--- src/main/java/Truck.java
+++ src/main/java/Truck.java
@@ -2,4 +2,13 @@
  * Created by Juergen on 04.12.2014.
  */
 public class Truck extends Vehicle {
+
+    private int axles;
+    private boolean adBlue;
+
+    public Truck(int axles, boolean adBlue) {
+        this.axles = axles;
+        this.adBlue = adBlue;
+
+    }
 }
