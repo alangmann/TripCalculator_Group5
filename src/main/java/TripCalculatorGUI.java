0c07706ac402f81a8b2854041cc99381fc96e2f5
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-12-11 12:26:46 +0100

    GUI created commit continued

--- src/main/java/TripCalculatorGUI.java
+++ src/main/java/TripCalculatorGUI.java
@@ -6,13 +6,40 @@ import java.awt.*;
  */
 public class TripCalculatorGUI extends JFrame {
 
-    public void initComponents()
-    {
+    public void initComponents() {
         Container con = this.getContentPane();
         con.setLayout(new BorderLayout());
         lblTitle = new JLabel("Tripcalculator Luttenberger u. Korosec");
         lblTitle.setHorizontalAlignment(JLabel.CENTER);
+
+        lblCO2onDistance = new JLabel("CO2 consumption based on distance:");
+        txtCO2onDistance = new JTextField();
+        txtCO2onDistance.setEditable(false);
+
+        lblCO2withSlope = new JLabel("CO2 consumption with slope:");
+        txtCO2withSlope = new JTextField();
+        txtCO2withSlope.setEditable(false);
+
+        lblCO2withRoutetype = new JLabel("CO2 consumption with route type");
+        txtCO2withRoutetype = new JTextField();
+        txtCO2withRoutetype.setEditable(false);
+
+        lblCO2fullCalculation = new JLabel("CO2 consumption full calculation");
+        txtCO2fullCalculation = new JTextField();
+        txtCO2fullCalculation.setEditable(false);
+
+        pnBigPanel = new JPanel();
+        pnBigPanel.setLayout(new GridLayout(10, 2, 5, 5));
+
+        pnBigPanel.add(lblCO2onDistance);
+        pnBigPanel.add(txtCO2onDistance);
+        pnBigPanel.add(lblCO2withSlope);
+        pnBigPanel.add(txtCO2withSlope);
+        pnBigPanel.add(lblCO2withRoutetype);
+        pnBigPanel.add(txtCO2withRoutetype);
+
         con.add(lblTitle, BorderLayout.NORTH);
+        con.add(pnBigPanel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
@@ -23,13 +50,12 @@ public class TripCalculatorGUI extends JFrame {
         tgui.setVisible(true);
         tgui.setLocationRelativeTo(null);
 
-
         TripCalculator tc = new TripCalculator();
         try {
             tc.readRoutesCSV();
-            System.out.println("CO2 on distance: " + tc.calculateCO2onDistance());
-            System.out.println("CO2 on distance and slope: " + tc.calculateCO2onDistanceAndSlope());
-            System.out.println("CO2 on route: " + tc.calculateCO2onRoute());
+            tgui.txtCO2onDistance.setText("" + tc.calculateCO2onDistance());
+            tgui.txtCO2withSlope.setText("" + tc.calculateCO2onDistanceAndSlope());
+            tgui.txtCO2withRoutetype.setText("" + tc.calculateCO2onRoute());
             System.out.println("CO2 on new distance: " + tc.calculateCO2onCar());
         } catch (Exception ex) {
 
@@ -37,4 +63,15 @@ public class TripCalculatorGUI extends JFrame {
     }
 
     private JLabel lblTitle;
+    private JPanel pnBigPanel;
+    private JLabel lblCO2onDistance;
+    private JTextField txtCO2onDistance;
+    private JLabel lblCO2withSlope;
+    private JTextField txtCO2withSlope;
+    private JLabel lblCO2withRoutetype;
+    private JTextField txtCO2withRoutetype;
+
+    private JLabel lblCO2fullCalculation;
+    private JTextField txtCO2fullCalculation;
+
 }
