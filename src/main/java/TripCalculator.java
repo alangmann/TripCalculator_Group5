6994930283e9b91f0e43fd3e89bb55d52f4623bb
lutjuc11 <lutjuc11@htlkaindorf.at>
2014-12-11 11:43:15 +0100

    GUI created commit

--- src/main/java/TripCalculator.java
+++ src/main/java/TripCalculator.java
@@ -97,20 +97,4 @@ public class TripCalculator {
     }
 
 
-
-
-    public static void main(String[] args) {
-        TripCalculator tc = new TripCalculator();
-        try {
-            tc.readRoutesCSV();
-            System.out.println("CO2 on distance: " + tc.calculateCO2onDistance());
-            System.out.println("CO2 on distance and slope: " + tc.calculateCO2onDistanceAndSlope());
-            System.out.println("CO2 on route: " + tc.calculateCO2onRoute());
-            System.out.println("CO2 on new distance: "+tc.calculateCO2onDistanceWithWeight());
-        } catch(Exception ex)
-        {
-
-        }
-    }
-
 }
---
+++ src/main/java/TripCalculatorGUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+/**
+ * Created by Juergen on 11.12.2014.
+ */
+public class TripCalculatorGUI {
+    private JTextField txtCO2distance;
+    private JLabel lblTitle;
+    private JLabel lblCO2distance;
+
+    public static void main(String[] args) {
+
+        TripCalculator tc = new TripCalculator();
+        try {
+            tc.readRoutesCSV();
+            System.out.println("CO2 on distance: " + tc.calculateCO2onDistance());
+            System.out.println("CO2 on distance and slope: " + tc.calculateCO2onDistanceAndSlope());
+            System.out.println("CO2 on route: " + tc.calculateCO2onRoute());
+            System.out.println("CO2 on new distance: " + tc.calculateCO2onDistanceWithWeight());
+        } catch (Exception ex) {
+
+        }
+    }
+}
